bf5238cf417a929f0a10916db6a6d9c1c9e0c16d
JimyCox <terraxted@hotmail.com>
2022-01-17 20:21:36 -0500

    inicio

---
+++ src/StoreApp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { AppRouter } from './config/router/AppRouter';
+
+function StoreApp() {
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
+}
+
+export default StoreApp;
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import StoreApp from './StoreApp';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StoreApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
